931bff1557f288c7a7e92a498f2785dcf1b1544d
GiancarloEBG <GiancarloEBG@protonmail.com>
2022-01-31 04:33:41 +0100

    using handleClick in different ways

--- src/Content.js
+++ src/Content.js
@@ -3,13 +3,35 @@ const Content = () => {
 		const names = ['Bob', 'Kevin', 'Gian'];
 		const int = Math.floor(Math.random() * 3);
 		return names[int];
-	  }
+	}
+
+    const handleClick = () => {
+        console.log('You clicked it')
+	}
+
+    const handleClick2 = (name) => {
+        console.log(`${name} was clicked`)
+	}
+
+    const handleClick3 = (e) => {
+        console.log(e.target.innerText)
+	}
 
 	return (
 			<main>
-				<p>
+				<p onDoubleClick={handleClick}>
+                    {/* when the function called has paramenters
+                    (use parenthesis) 
+                    it is called inmediately */}
 					Hello {handleNameChange()}!
 				</p>
+                {/* does not have "()" to do not make the call inmediately */}
+				<button onClick={handleClick}>Click it</button>
+                {/* the following button has an onclick function with paramenters
+                but it is not called inmediately because it has an annonymous
+                function calling it, that it starts when the onclick is done */}
+				<button onClick={() => handleClick2('Gian')}>Click it</button>
+				<button onClick={(e) => handleClick3(e)}>Click it</button>
 			</main>
 	)
 };
